dcef75de4a4da4fccad93aa86ec4873eb787be56
Nanson Augusto <nanson.augusto@hotmail.com>
2013-10-08 21:25:29 -0300

    adição do teste do controlador.

---
+++ ControleAtleta/test/controleatleta/test/ControleJgBasqueteTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleatleta.test;
+
+import controleatleta.ControleJogadorBasquete;
+import controleatleta.JogadorBasquete;
+import junit.framework.Assert.*;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author nanson
+ */
+public class ControleJgBasqueteTest extends TestCase {
+    
+    private ControleJogadorBasquete controle;
+
+    @Override
+    protected void setUp() throws Exception {
+        controle = new ControleJogadorBasquete();
+    }
+    
+    public void testConstrutor(){
+        assertNotNull(controle.getListaJogadores());
+    }
+    
+    public void testPesquisar(){
+        for (int i = 0; i < 10; i++) {
+            JogadorBasquete jogadorBasquete = new JogadorBasquete(Integer.toString(i)+"teste");
+            controle.adicionar(jogadorBasquete);
+        }
+        
+        assertNotNull(controle.pesquisar("5teste"));
+        assertNull(controle.pesquisar("teste"));
+    }
+}
